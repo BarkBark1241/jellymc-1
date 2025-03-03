f97b70f28c77e6ebe68701362ebe4e2cb26d6977
mathiasclari <mathiasclari@noximity.com>
2025-03-03 01:55:11 +0100

    Potential Fix #1

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',
+  },
+  // This ensures that the router works properly in production
+  // by returning index.html for all routes
+  preview: {
+    port: 4173
+  }
 })
